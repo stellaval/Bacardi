bb4dfb62cb38524aed34adef06825a34a19bb20d
Emil <skllt75@gmail.com>
2015-07-25 01:08:22 +0300

    Score...the beginning

--- assets/scripts/game.js
+++ assets/scripts/game.js
@@ -4,17 +4,18 @@ var platforms,
     cocktail,
     cocktailGroup,
     cocktailCounter = 0,
-    score;
+    scoreText,
+    score = 0;
 
 var Game = {
-    preload: function() {
+    preload: function () {
         game.load.image('sky', 'assets/images/sky.png');
         game.load.image('ground', 'assets/images/platform.png');
         game.load.image('cocktail', 'assets/images/cocktail.png');
         game.load.spritesheet('dude', 'assets/images/dude.png', 32, 48);
         game.load.image('bottom', 'assets/images/bottom.png');
     },
-    create: function() {
+    create: function () {
         var ground,
             background,
             sky,
@@ -83,11 +84,9 @@ var Game = {
 
         game.camera.follow(player, Phaser.Camera.FOLLOW_TOPDOWN);
 
-        //Show score in game world  - DOES NOT SHOW !! )):  used: http://phaser.io/examples/v2/text/update-text 
-        score = game.add.text(game.world.width / 2, 0, 'SCORE ');
-        score.anchor.setTo(0.5, 0.5);
+        scoreText = game.add.text(10, game.world.height - 500, 'Score: 0', { textSize: '22px', fill: '#fff' });
     },
-    update: function() {
+    update: function () {
         game.physics.arcade.collide(player, platforms);
         game.physics.arcade.collide(player, cocktailGroup, collisionHandler, null);
 
@@ -116,11 +115,8 @@ var Game = {
         function collisionHandler(player, cocktail) {
             cocktailCounter++;
             cocktail.kill();
-            updateScore();
-        }
-
-        function updateScore() {
-            score.setText('SCORE ' + cocktailCounter);
+            score += 10;
+            scoreText.text = 'Score: ' + score;
         }
     }
 }
