f0e3479ae8c08d4c0eeef799e0dbf0aac2e22670
Ekaterina Martynyuk <martynyuk88@gmail.com>
2015-07-25 00:54:10 +0300

    conflicts resolved

--- assets/scripts/game_over.js
+++ assets/scripts/game_over.js
@@ -10,10 +10,10 @@ var Game_Over = {
     create : function() {
 
         // Create button to start game like in Menu.
-        this.add.button(0, 0, 'gameover', this.startGame, this);
+        //this.add.button(0, 0, 'gameover', this.startGame, this);
 
         // Add text with information about the score from last game.
-        game.add.text(235, 350, "LAST SCORE", { font: "bold 16px sans-serif", fill: "#46c0f9", align: "center"});
+        game.add.text(235, game.world.height - 100, "LAST SCORE", { font: "bold 16px sans-serif", fill: "#ffffff", align: "center"});
         game.add.text(350, 348, score.toString(), { font: "bold 20px sans-serif", fill: "#fff", align: "center" });
 
     },
@@ -21,7 +21,7 @@ var Game_Over = {
     startGame: function () {
 
         // Change the state back to Game.
-        this.state.start('Game');
+        game.state.start('Game');
 
     }
 
--- assets/scripts/main.js
+++ assets/scripts/main.js
@@ -6,5 +6,6 @@ game.state.add('Menu', Menu);
 
 // Adding the Game state.
 game.state.add('Game', Game);
+game.state.add('Game_Over', Game_Over);
 
 game.state.start('Game');
