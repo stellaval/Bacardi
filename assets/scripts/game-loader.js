86a936d411a66d404f7916d118bf794ddaab5214
Ekaterina Martynyuk <martynyuk88@gmail.com>
2015-07-31 11:56:15 +0300

    rewrited game-loader.js

--- assets/scripts/game-loader.js
+++ assets/scripts/game-loader.js
@@ -1,34 +1,10 @@
-function loadJS(file) {
+var container = document.getElementById('container');
 
-    var jsElm = document.createElement("script");
-    jsElm.type = "application/javascript";
-    jsElm.src = file;
-    // finally insert the element to the body element in order to load the script
-    document.body.appendChild(jsElm);
+function loadGame() {
+    var js = document.createElement('script');
+    js.setAttribute('src', 'assets/scripts/main.js');
+    document.body.appendChild(js);
+    document.body.removeChild(container);
 }
 
-function loadAllGame(){
-
-    loadJS('assets/scripts/main.js');
-}
-
-// create a one-time event
-function onetime(node, type, callback) {
-
-    node.addEventListener(type, function(e) {
-        // remove event
-        e.target.removeEventListener(e.type, arguments.callee);
-        // call handler
-        return callback(e);
-    });
-}
-
-function handler(e) {
-    loadAllGame();
-    var parent = document.getElementsByTagName('body')[0];
-    var child = document.getElementById('container');
-    parent.removeChild(child);
-}
-
-// one-time event initializing
-onetime(document.getElementById("container"), "click", handler);
+container.addEventListener("click", loadGame);
